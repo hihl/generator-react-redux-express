c5dc0bcf0aae653bf6a825bb72e8a81e93d3f20c
zhengfeng.yao <zhengfeng.yao@tongdun.cn>
2016-08-25 18:27:56 +0800

    ajust

---
+++ generators/app/prompts.js
@@ -0,0 +1,11 @@
+'use strict';
+const utils = require('../../utils');
+
+module.exports = [
+  {
+    type: 'input',
+    name: 'appName',
+    message: 'Please choose your application name',
+    default: utils.yeoman.getAppName()
+  }
+];
